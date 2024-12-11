08e96250d600a078d18f7cc54b17634750f6bb5a
Nurhat Menteş <nurhatmentes@gmail.com>
2024-12-11 15:03:40 +0300

    TradingView infrastructure has been created.

---
+++ AutoTraderApp.Application/Features/TradingView/DTOs/TradingViewStrategyDto.cs
@@ -0,0 +1,11 @@
+﻿namespace AutoTraderApp.Application.Features.TradingView.DTOs;
+
+public class TradingViewStrategyDto
+{
+    public string Name { get; set; } = null!;
+    public string Symbol { get; set; } = null!;
+    public decimal EntryPrice { get; set; }
+    public decimal StopLoss { get; set; }
+    public decimal TakeProfit { get; set; }
+    public string TimeFrame { get; set; } = null!;
+}
